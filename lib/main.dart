3403ab5d23f1905af4c3a41cd0bfb2e62d18808d
SMART24\User <junhek2525@gmail.com>
2025-03-24 16:12:29 +0900

    commit commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/drawer.dart';
+import 'package:untitled/setting_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/setting': (constext) => const SettingPage()
+      },
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("안녕하세요"),
+      ),
+      drawer: const MyDrawer(),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.red,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.yellow,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
